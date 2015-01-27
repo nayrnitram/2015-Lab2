5261a4cbac2b36e7dade8ffa2811ba514a1f0c06
nayrnitram <rc18mart@siena.edu>
2015-01-26 19:04:08 -0500

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -3,8 +3,6 @@ public class Hello
   public static void main(String args[])
   {
     System.out.println("Hello, my name is Ryan");
-    System.out.println("Hello, my name is Sal");
-    System.out.println("Hello, my name is Mary");
-    System.out.println("Hello, my name is Anna");
+   
   }
 }
